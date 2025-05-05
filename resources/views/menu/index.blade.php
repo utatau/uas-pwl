d006378348a62cd0a7a9400148372afa420cb10c
Putra Mahardika <putramahardika276@gmail.com>
2025-05-05 12:35:48 +0800

    fix view

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -17,16 +17,17 @@
 </header>
 <section class="shop container">
     <h2 class="section-title">Products</h2>
-    <?php foreach ($data as $d):?>
     <div class="shop-content">
+<?php foreach($data as $d):?>
         <div class="product-box">
-            <img src="storage/<?= $d->gambar?>"  alt="menu" class="product-img">
+            <img src="storage/<?= $d->gambar?>"  alt="menu" style="min-height: 10vh;" class="product-img">
             <h2 class="product-title">
                 {{$d->nama_menu}}
             </h2>
             <span class="price">{{$d->harga_menu}}</span>
             <i class="bx bx-shopping-bag add-cart"></i>
-            <?php endforeach;?>
         </div>
-    </div>
+
+<?php endforeach;?>
+</div>
 </section>
